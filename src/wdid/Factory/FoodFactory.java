5e0ce211758647ead69a50ba2ab4c061814c389e
HasanSaleemi <hasansaleemi@gmail.com>
2018-11-18 17:14:37 -0600

    Food API working.

--- src/wdid/Factory/FoodFactory.java
+++ src/wdid/Factory/FoodFactory.java
@@ -1,8 +1,15 @@
 package wdid.Factory;
 
+import com.google.appengine.repackaged.com.google.gson.JsonArray;
+import com.google.appengine.repackaged.com.google.gson.JsonElement;
+import com.google.appengine.repackaged.com.google.gson.JsonObject;
+import com.google.appengine.repackaged.com.google.gson.JsonParser;
 import wdid.Recommendations.*;
 import wdid.Users.User;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,14 +18,39 @@ public class FoodFactory implements RecommendationFactory{
     @Override
     public RecommendationIterator getRecommendations(User user) {
         return new RecommendationIterator(getData());
-        // TODO: Add API here
     }
 
     public List<Recommendation> getData(){
+        StringBuffer json = new StringBuffer();
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.288189,-97.735487&radius=1500&type=restaurant&key=AIzaSyCjJ9D9zTYhgy0ffR_7sNdHQuzue__ailA");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                json.append(line);
+            }
+            reader.close();
+        } catch(Exception ignored){}
+
+        JsonObject parseJSON = new JsonParser().parse(json.toString()).getAsJsonObject();
+        JsonArray results = parseJSON.get("results").getAsJsonArray();
+        int size = results.size();
+
         List<Recommendation> res = new ArrayList<>();
-        Location location = new Location();
-        Recommendation food = new Food("Mac D's", location, "sux", 4.9);
-        res.add(food);
+        for(int i = 0; i < size; i++){
+            JsonObject grab = results.get(i).getAsJsonObject();
+
+            JsonElement title = grab.get("name");
+            JsonElement desc  = grab.get("vicinity");
+            JsonElement rating  = grab.get("rating");
+
+            if(title != null && desc != null && rating != null){
+                Recommendation food = new Food(title.getAsString(), new Location(), desc.getAsString(), rating.getAsDouble());
+                res.add(food);
+            }
+        }
+
         return res;
     }
 }
